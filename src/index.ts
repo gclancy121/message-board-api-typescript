1c8390b32500ca878442c6c7c3e90dcd39a51553
Gage Clancy <gclancy121@gmail.com>
2023-02-07 14:26:55 -0600

    barebones server setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-import express from "express";
+import server from './server';
+
+const PORT:string | number = process.env.port || 9000;
+server.listen(PORT, () => {
+    console.log(`---Server listening on port ${PORT }---`);
+})
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from 'cors';
+import helmet from 'helmet';
+
+const server:Express = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+export default server;
