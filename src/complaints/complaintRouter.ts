724c0dcaff6b41f16390d384c96f376df29d0c64
Gage Clancy <gclancy121@gmail.com>
2023-02-08 14:29:05 -0600

    added rudimentary error handling, will fix later

--- src/complaints/complaintRouter.ts
+++ src/complaints/complaintRouter.ts
@@ -7,7 +7,7 @@ const router:Router = express.Router();
 router.post('/', checkPayload, (req:Request, res:Response, next:NextFunction) => {
     Complaints.addComplaint(req.body).then(result => {
         res.status(201).json(result);
-    })
+    }).catch(err => console.log(err));
 })
 
 export {router as complaintRouter};
