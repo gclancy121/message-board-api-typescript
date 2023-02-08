f80354655d134416dc367e31c8f5567505d71c5a
Gage Clancy <gclancy121@gmail.com>
2023-02-08 15:03:58 -0600

    added data typing to parts of the router

--- src/complaints/complaintRouter.ts
+++ src/complaints/complaintRouter.ts
@@ -1,13 +1,13 @@
-import express, {Router, Request, Response, NextFunction} from 'express';
+import express, {Router, Request, Response} from 'express';
 import * as Complaints from './complaintModel';
 import checkPayload from './complaintMiddleware';
 
 const router:Router = express.Router();
 
-router.post('/', checkPayload, (req:Request, res:Response, next:NextFunction) => {
-    Complaints.addComplaint(req.body).then(result => {
+router.post('/', checkPayload, (req:Request, res:Response) => {
+    Complaints.addComplaint(req.body).then((result:string) => {
         res.status(201).json(result);
-    }).catch(err => console.log(err));
+    }).catch((err:Error) => console.error(err));
 })
 
 export {router as complaintRouter};
